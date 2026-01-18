16196897f2d019ab9303ea3bce1e0026ec7055a5
Frank Parejo <francisco.parejo.lopez@gmail.com>
2026-01-18 23:41:06 +0100

    refactor node implementation

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,6 @@ use uuid::Uuid;
 
 #[derive(Debug, Default)]
 struct NodeState {
-    node_id: String,
     messages: HashSet<i32>,
     /// It represents a vector of node ids. These nodes will be used for gossiping.
     neighbors: Vec<String>,
@@ -19,12 +18,49 @@ struct NodeState {
 
 #[derive(Debug)]
 struct Node {
+    node_id: String,
+    node_ids: Vec<String>,
     state: Arc<Mutex<NodeState>>,
 }
 
 impl<'a> Node {
-    fn new(state: Arc<Mutex<NodeState>>) -> Self {
-        Self { state }
+    fn init(line: String) -> anyhow::Result<Self> {
+        let msg: Message = serde_json::from_str(&line).context("Message deserialization error")?;
+
+        match msg.body.clone() {
+            MessageBody::Init {
+                msg_id,
+                node_id,
+                node_ids,
+            } => {
+                let state = Arc::new(Mutex::new(NodeState::default()));
+                let node = Self {
+                    node_id,
+                    node_ids,
+                    state: state.clone(),
+                };
+
+                let mut state_guard = state.lock().expect("State poisoned when executing init");
+
+                let response = node.build_reply(
+                    &msg,
+                    &mut state_guard,
+                    Some(MessageBody::InitOk {
+                        in_reply_to: msg_id,
+                    }),
+                );
+
+                let json =
+                    serde_json::to_string(&response).context("Message serialization error")?;
+
+                println!("{}", json);
+
+                anyhow::Ok(node)
+            }
+            _ => Err(anyhow::anyhow!(
+                "Init message is not the first message received"
+            )),
+        }
     }
 
     fn reply(&mut self, req: Message) -> anyhow::Result<Option<Message>> {
@@ -34,17 +70,6 @@ impl<'a> Node {
             .expect("State poisoned while building reply");
 
         let body: Option<MessageBody> = match req.body.clone() {
-            MessageBody::Init {
-                msg_id,
-                node_id,
-                node_ids: _,
-            } => {
-                state.node_id = node_id;
-
-                Some(MessageBody::InitOk {
-                    in_reply_to: msg_id,
-                })
-            }
             MessageBody::Echo { msg_id, echo } => Some(MessageBody::EchoOk {
                 msg_id: state.last_message_id + 1,
                 in_reply_to: msg_id,
@@ -95,7 +120,7 @@ impl<'a> Node {
         state.last_message_id += 1;
 
         Some(Message {
-            src: state.node_id.clone(),
+            src: self.node_id.clone(),
             dest: req.src.clone(),
             body: body?,
         })
@@ -164,10 +189,17 @@ enum MessageBody {
 }
 
 fn main() -> anyhow::Result<()> {
-    let state = Arc::new(Mutex::new(NodeState::default()));
-    let mut node = Node::new(state.clone());
+    let mut lines = io::stdin().lines();
 
-    for line in io::stdin().lines() {
+    // The first line must be a init, otherwise it returns an error.
+    let mut node = match lines.next() {
+        Some(line) => Node::init(line?)?,
+        None => {
+            panic!("Init message is required")
+        }
+    };
+
+    for line in lines {
         let content = line?;
 
         let msg: Message =
@@ -183,13 +215,12 @@ fn main() -> anyhow::Result<()> {
                 MessageBody::Broadcast { msg_id: _, message } => {
                     let state = node.state.lock().expect("State poisoned");
                     let neighbors = state.neighbors.clone();
+                    let node_id = node.node_id.clone();
 
-                    for neighbor in neighbors {
-                        let node_id = state.node_id.clone();
-
-                        thread::spawn(move || -> anyhow::Result<()> {
+                    thread::spawn(move || -> anyhow::Result<()> {
+                        for neighbor in neighbors {
                             let message = Message {
-                                src: node_id,
+                                src: node_id.clone(),
                                 dest: neighbor,
                                 body: MessageBody::Broadcast {
                                     msg_id: None,
@@ -201,10 +232,10 @@ fn main() -> anyhow::Result<()> {
                                 .context("Message serialization error")?;
 
                             println!("{}", json);
+                        }
 
-                            Ok(())
-                        });
-                    }
+                        Ok(())
+                    });
                 }
                 _ => (),
             };
