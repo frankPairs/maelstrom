d973b2a62f23674dc289e2ef9b3d0bd34ca01ab8
Frank Parejo <francisco.parejo.lopez@gmail.com>
2026-01-18 01:07:32 +0100

    implement multi-node broadcast solution

--- src/main.rs
+++ src/main.rs
@@ -1,122 +1,115 @@
-use std::{
-    collections::HashMap,
-    io::{self},
-};
-
 use anyhow::Context;
 use serde::{Deserialize, Serialize};
+use std::{
+    collections::{HashMap, HashSet},
+    io::{self},
+    sync::{Arc, Mutex},
+    thread,
+};
 use uuid::Uuid;
 
 #[derive(Debug, Default)]
 struct NodeState {
     node_id: String,
-    messages: Vec<i32>,
+    messages: HashSet<i32>,
+    /// It represents a vector of node ids. These nodes will be used for gossiping.
+    neighbors: Vec<String>,
     last_message_id: u32,
 }
 
 #[derive(Debug)]
-struct Node<'a> {
-    state: &'a mut NodeState,
+struct Node {
+    state: Arc<Mutex<NodeState>>,
 }
 
-impl<'a> Node<'a> {
-    fn new(state: &'a mut NodeState) -> Self {
+impl<'a> Node {
+    fn new(state: Arc<Mutex<NodeState>>) -> Self {
         Self { state }
     }
 
-    fn reply(&mut self, msg: Message) -> Message {
-        match msg.body {
+    fn reply(&mut self, req: Message) -> anyhow::Result<Option<Message>> {
+        let mut state = self
+            .state
+            .lock()
+            .expect("State poisoned while building reply");
+
+        let body: Option<MessageBody> = match req.body.clone() {
             MessageBody::Init {
                 msg_id,
                 node_id,
                 node_ids: _,
             } => {
-                self.state.node_id = node_id;
+                state.node_id = node_id;
 
-                Message {
-                    src: self.state.node_id.clone(),
-                    dest: msg.src,
-                    body: MessageBody::InitOk {
-                        in_reply_to: msg_id,
-                    },
-                }
+                Some(MessageBody::InitOk {
+                    in_reply_to: msg_id,
+                })
             }
-            MessageBody::Echo { msg_id, echo } => {
-                self.state.last_message_id += 1;
-
-                Message {
-                    src: self.state.node_id.clone(),
-                    dest: msg.src,
-                    body: MessageBody::EchoOk {
-                        msg_id: self.state.last_message_id,
-                        in_reply_to: msg_id,
-                        echo,
-                    },
-                }
-            }
-            MessageBody::Generate { msg_id } => {
-                self.state.last_message_id += 1;
-
-                Message {
-                    src: self.state.node_id.clone(),
-                    dest: msg.src,
-                    body: MessageBody::GenerateOk {
-                        in_reply_to: msg_id,
-                        id: Uuid::new_v4(),
-                    },
-                }
-            }
+            MessageBody::Echo { msg_id, echo } => Some(MessageBody::EchoOk {
+                msg_id: state.last_message_id + 1,
+                in_reply_to: msg_id,
+                echo,
+            }),
+            MessageBody::Generate { msg_id } => Some(MessageBody::GenerateOk {
+                msg_id: state.last_message_id + 1,
+                in_reply_to: msg_id,
+                id: Uuid::new_v4(),
+            }),
             MessageBody::Broadcast { msg_id, message } => {
-                self.state.last_message_id += 1;
-                self.state.messages.push(message);
+                state.messages.insert(message);
 
-                Message {
-                    src: self.state.node_id.clone(),
-                    dest: msg.src,
-                    body: MessageBody::BroadcastOk {
-                        in_reply_to: msg_id,
-                    },
+                if let Some(in_reply_to) = msg_id {
+                    Some(MessageBody::BroadcastOk {
+                        in_reply_to,
+                        msg_id: state.last_message_id + 1,
+                    })
+                } else {
+                    None
                 }
             }
-            MessageBody::Read { msg_id } => {
-                self.state.last_message_id += 1;
+            MessageBody::Read { msg_id } => Some(MessageBody::ReadOk {
+                msg_id: state.last_message_id + 1,
+                messages: state.messages.clone(),
+                in_reply_to: msg_id,
+            }),
+            MessageBody::Topology { msg_id, topology } => {
+                state.neighbors = topology.keys().map(|k| k.to_string()).collect();
 
-                Message {
-                    src: self.state.node_id.clone(),
-                    dest: msg.src,
-                    body: MessageBody::ReadOk {
-                        messages: self.state.messages.clone(),
-                        in_reply_to: msg_id,
-                    },
-                }
+                Some(MessageBody::TopologyOk {
+                    msg_id: state.last_message_id + 1,
+                    in_reply_to: msg_id,
+                })
             }
-            MessageBody::Topology {
-                msg_id,
-                topology: _,
-            } => {
-                self.state.last_message_id += 1;
+            body => unimplemented!("Message {:?} not implemented yet", body),
+        };
 
-                Message {
-                    src: self.state.node_id.clone(),
-                    dest: msg.src,
-                    body: MessageBody::TopologyOk {
-                        in_reply_to: msg_id,
-                    },
-                }
-            }
-            _ => unimplemented!("Message reply not implemented yet"),
-        }
+        Ok(self.build_reply(&req, &mut state, body))
+    }
+
+    fn build_reply(
+        &self,
+        req: &Message,
+        state: &mut NodeState,
+        body: Option<MessageBody>,
+    ) -> Option<Message> {
+        state.last_message_id += 1;
+
+        Some(Message {
+            src: state.node_id.clone(),
+            dest: req.src.clone(),
+            body: body?,
+        })
     }
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 struct Message {
     src: String,
     dest: String,
     body: MessageBody,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 #[serde(tag = "type", rename_all = "snake_case")]
 enum MessageBody {
     Echo {
@@ -140,21 +133,24 @@ enum MessageBody {
         msg_id: u32,
     },
     GenerateOk {
+        msg_id: u32,
         in_reply_to: u32,
         id: Uuid,
     },
     Broadcast {
-        msg_id: u32,
+        msg_id: Option<u32>,
         message: i32,
     },
     BroadcastOk {
+        msg_id: u32,
         in_reply_to: u32,
     },
     Read {
         msg_id: u32,
     },
     ReadOk {
-        messages: Vec<i32>,
+        msg_id: u32,
+        messages: HashSet<i32>,
         in_reply_to: u32,
     },
     Topology {
@@ -162,25 +158,57 @@ enum MessageBody {
         topology: HashMap<String, Vec<String>>,
     },
     TopologyOk {
+        msg_id: u32,
         in_reply_to: u32,
     },
 }
 
 fn main() -> anyhow::Result<()> {
-    let mut state = NodeState::default();
-    let mut node = Node::new(&mut state);
+    let state = Arc::new(Mutex::new(NodeState::default()));
+    let mut node = Node::new(state.clone());
 
     for line in io::stdin().lines() {
         let content = line?;
 
-        let message: Message =
+        let msg: Message =
             serde_json::from_str(&content).context("Message deserialization error")?;
 
-        let response = node.reply(message);
+        if let Some(response) = node.reply(msg.clone())? {
+            let json = serde_json::to_string(&response).context("Message serialization error")?;
 
-        let json = serde_json::to_string(&response).context("Message serialization error")?;
+            println!("{}", json);
 
-        println!("{}", json);
+            // Gossiping messages to neighbour
+            match msg.body {
+                MessageBody::Broadcast { msg_id: _, message } => {
+                    let state = node.state.lock().expect("State poisoned");
+                    let neighbors = state.neighbors.clone();
+
+                    for neighbor in neighbors {
+                        let node_id = state.node_id.clone();
+
+                        thread::spawn(move || -> anyhow::Result<()> {
+                            let message = Message {
+                                src: node_id,
+                                dest: neighbor,
+                                body: MessageBody::Broadcast {
+                                    msg_id: None,
+                                    message,
+                                },
+                            };
+
+                            let json = serde_json::to_string(&message)
+                                .context("Message serialization error")?;
+
+                            println!("{}", json);
+
+                            Ok(())
+                        });
+                    }
+                }
+                _ => (),
+            };
+        }
     }
 
     Ok(())
